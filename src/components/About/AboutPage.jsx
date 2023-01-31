fbeabc926a98a95bc9992f0280701d62fdb5a9a6
Marieke <69918471+Makie2020@users.noreply.github.com>
2023-01-31 09:11:57 +0100

    Adjustment About Page

--- src/components/About/AboutPage.jsx
+++ src/components/About/AboutPage.jsx
@@ -9,6 +9,7 @@ function AboutPage() {
           <h2 className="section__header">About Me</h2>
           <p>Hi, my name is Marieke Linneman, a  Junior Full-Stack Web Developer from Palma de Mallorca, Spain.</p><br/>
           <p>The technlogies I work with are HTML, Javascript, Css and React.</p>
+          <p>Eager to learn new skills/ technlogies towards developing and problem-solving. Love to face new challenges and having the courage to admit mistakes and learn from them! Learning fast and loving to help makes me a good team player.</p>
         </div>
         <div className="about__image"><img src={"../../Portfolio/assets/images/Photo Marieke.jpg"} alt='Marieke' id='img'/></div>
       </section>
